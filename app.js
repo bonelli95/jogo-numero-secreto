fb1fdd0862eb7e42176aab21149a3e4ab4f63bea
Rodrigo Bonelli <159248066+bonelli95@users.noreply.github.com>
2024-04-01 17:59:11 +0100

    Correction in incrementation

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ function limparCampo() {
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
-    tentativas = 1;
+    tries = 1;
     exibirMensagemInicial();
     document.getElementById("reiniciar").setAttribute("disabled", true);
 }
